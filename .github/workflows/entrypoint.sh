bc6ba5dffd4dd1ad5cedc35af91636de0105f0c1
tarun00000 <tarunvats1990@gmail.com>
2022-08-03 14:27:27 -0400

    Update entrypoint.sh

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -uex
 
+if ["$GITHUB_EVENT_NAME" != "milestone"]; then
+  echo "::debug:: the event name was '$GITHUB_EVENT_NAME'"
+  exit 0
+fi
+
+event_type=$(jq --raw-output .action $GITHUB_EVENT_PATH)
+
+if [$event_type != "closed" ]; then
+  echo "::debug:: the event type was '$event_type'"
+  exit 0
+fi
+
+milestone_name=$(jq --raw-output .milestone.title $GITHUB_EVENT_PATH)
+
 echo "::set-output name=release-url::http://example.com"
 
 exit 0
